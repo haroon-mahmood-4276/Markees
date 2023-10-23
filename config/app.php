7420b47553250de9caf24797baa6de159eda3ff0
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2023-10-24 00:10:20 +0500

    Composer Packages Installed

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -168,6 +169,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TenancyServiceProvider::class,
     ])->toArray(),
 
     /*
